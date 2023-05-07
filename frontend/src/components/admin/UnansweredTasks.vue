7395be69ca2cdda5411cf51ab915f6dcbe74a57d
Grapensparr <liza.grapensparr@gmail.com>
2023-05-07 13:57:21 +0200

    Printing tasks to be voted for

--- frontend/src/components/admin/UnansweredTasks.vue
+++ frontend/src/components/admin/UnansweredTasks.vue
@@ -1,10 +1,45 @@
 <script setup lang="ts">
+  import { socket } from '@/socket';
+  import { ref } from 'vue';
 
+  interface Task {
+    _id: string;
+    task: string;
+    storyPoints: number;
+    deleted: boolean;
+  }
+
+  const tasksArray = ref<Task[]>([]);
+
+  function fetchTasks() {
+    fetch('http://localhost:3000/tasks')
+    .then(response => {
+      console.log('Response:', response);
+      return response.json();
+    })
+    .then(tasks => {
+      const filteredTasks = tasks.filter((task: any) => task.storyPoints === 0 && task.deleted === false);
+      tasksArray.value = filteredTasks;
+      console.log(filteredTasks)
+    })
+    .catch(error => {
+      console.error('Error fetching tasks:', error);
+    });
+  }
+
+  socket.on('updateList', () => {
+    fetchTasks();
+  });
+
+  fetchTasks();
 </script>
 
 <template>
   <div class="unansweredTasksContainer">
     <h2 class="adminCenter">To be voted on:</h2>
+    <ul>
+      <li v-for="(task, index) in tasksArray" :key="index">{{ task.task }}</li>
+    </ul>
   </div>
 </template>
 
