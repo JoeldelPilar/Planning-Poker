1d15e8718419ff4c2fedba61805c14afd29f8fcf
Grapensparr <liza.grapensparr@gmail.com>
2023-05-11 17:35:51 +0200

    Scorecards added

---
+++ frontend/src/components/UserScorecards.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  defineProps({
+    name: String,
+    storyPoints: String
+  })
+</script>
+
+<template>
+  <div class="userScorecard">
+    {{ name }} <span>{{ storyPoints }}</span>
+  </div>
+</template>
+
+<style>
+  .userScorecard {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: lightgray;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    font-size: 1.5rem;
+    width: 150px;
+    height: 200px;
+    border-radius: 10px;
+  }
+</style>
--- frontend/src/views/UserView.vue
+++ frontend/src/views/UserView.vue
@@ -4,11 +4,19 @@ import PreviousTask from '@/components/PreviousTask.vue'
 import UsercardView from '../components/UserCard.vue'
 import ResultCard from '@/components/ResultCard.vue'
 import UserTask from '@/components/UserTask.vue'
+import UserScorecards from '@/components/UserScorecards.vue'
+import { userState } from '@/sockets/userSocket';
 import { socket } from '@/socket'
-import { ref } from 'vue'
+import { ref, watchEffect } from 'vue'
   
-const fibonaccis = ["1", "3", "5", "8", "?"];
+const connectedUsers = ref(userState.users);
 
+watchEffect(() => {
+  connectedUsers.value = userState.users;
+});
+
+const fibonaccis = ["1", "3", "5", "8", "?"];
+const task = ref('')
 
   const disabledCards = ref(true)
 
@@ -25,13 +33,6 @@ const fibonaccis = ["1", "3", "5", "8", "?"];
   socket.on("disableBtn", () => {
     disabledCards.value = true;
   })
-
-  const signedInUser = localStorage.getItem("user")
-  console.log("signedInUser", signedInUser)
-
-    });
-  }
-
 
 const signedInUser = localStorage.getItem('user');
 console.log('signedInUser', signedInUser);
@@ -63,7 +64,14 @@ socket.on('redirectToStartingpage', (startingpage) => {
         :key="index"
         :fibonacci-value="fibonacci"
         :disabled="disabledCards === true"
-
+      />
+    </div>
+    <div class="userScorecards">
+      <UserScorecards
+        v-for="(user, index) in connectedUsers"
+        :key="index"
+        :name="user.name"
+        :story-points="user.storyPoints"
       />
     </div>
   </main>
@@ -104,5 +112,12 @@ socket.on('redirectToStartingpage', (startingpage) => {
 .result-container {
   display: flex;
   justify-content: end;
+}
+
+.userScorecards {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin-top: 20px;
 }
 </style>
